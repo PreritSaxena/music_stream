422868ad0fa1eabfba351fc1506977721e66ca6d
Prerit Saxena <112962509+PreritSaxena@users.noreply.github.com>
2024-07-23 00:13:31 +0530

    Update App.jsx

--- spotify admin/src/App.jsx
+++ spotify admin/src/App.jsx
@@ -10,7 +10,7 @@ import Sidebar from './components/Sidebar';
 import Navbar from './components/Navbar';
 
 
- export const url = 'http://localhost:4000';
+ export const url = 'https://music-stream-backend.onrender.com';
 const App = () => {
 
   return (
